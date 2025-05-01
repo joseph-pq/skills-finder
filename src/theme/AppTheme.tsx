ecd43efde08250ff763dbae18992d9b499a8b90a
Joseph P. <joseph.pena.q@gmail.com>
2025-04-30 22:19:10 -0300

    💄 foramt src/theme/AppTheme.tsx

--- src/theme/AppTheme.tsx
+++ src/theme/AppTheme.tsx
@@ -1,45 +1,45 @@
-import { createTheme } from '@mui/material/styles';
+import { createTheme } from "@mui/material/styles";
 
 const themeOptions = createTheme({
   palette: {
-    mode: 'dark',
+    mode: "dark",
     primary: {
-      main: '#D9D137',
+      main: "#D9D137",
     },
     secondary: {
-      main: '#60BAD4',
+      main: "#60BAD4",
     },
     text: {
-      primary: '#D9D137',
+      primary: "#D9D137",
     },
     background: {
-      default: '#030001',
-      paper: '#030001',
+      default: "#030001",
+      paper: "#030001",
     },
     info: {
-      main: '#262730',
+      main: "#262730",
     },
     error: {
-      main: '#D33F49',
+      main: "#D33F49",
     },
     warning: {
-      main: '#ffa726',
+      main: "#ffa726",
     },
   },
   components: {
     MuiCard: {
       styleOverrides: {
         root: {
-          border: '3px solid #D9D137', // Uses primary color for border
-          borderRadius: '8px', // Optional: rounded corners
+          border: "3px solid #D9D137", // Uses primary color for border
+          borderRadius: "8px", // Optional: rounded corners
         },
       },
     },
     MuiTableContainer: {
       styleOverrides: {
         root: {
-          border: '3px solid #D9D137', // Border for tables
-          borderRadius: '8px', // Optional
+          border: "3px solid #D9D137", // Border for tables
+          borderRadius: "8px", // Optional
         },
       },
     },
